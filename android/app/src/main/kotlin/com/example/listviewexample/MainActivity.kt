339e8e571126037f778d95b8e57a2f1c5efe26dd
Muhammad Shakir khan <shakir.sedoc@gmail.com>
2022-09-28 17:38:15 +0500

    Feat: List View & Grid View Builder Example

---
+++ android/app/src/main/kotlin/com/example/listviewexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listviewexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/list_view_example.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatelessWidget {
+  const ListViewScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple List View Example"),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 1"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 2"),
+          ),          
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 3"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 4"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 5"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 6"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 7"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 8"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 9"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 10"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 11"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 12"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 13"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 14"),
+          ),
+          ListTile(
+            trailing: Icon(Icons.arrow_circle_right),
+            subtitle: Text("Sub Title"),
+            title: Text("List Item 15"),
+          )
+        ],
+      ),
+    );
+  }
+}
